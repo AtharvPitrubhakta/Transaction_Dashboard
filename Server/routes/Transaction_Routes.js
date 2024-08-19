a9ed1367b97991caaa1c8a61a6051b78fa28ccf2
Atharv Pitrubhakta <74965270+AtharvPitrubhakta@users.noreply.github.com>
2024-08-19 10:14:03 +0530

    Update Transaction_Routes.js

--- Server/routes/Transaction_Routes.js
+++ Server/routes/Transaction_Routes.js
@@ -2,13 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { fetchApi } = require("../controllers/Transaction");
-const {listTransactions, getStatistics, getPriceRangeDistribution, getCategoryDistribution, getCombinedData} = require("../controllers/Transaction");
+const {getCombinedData} = require("../controllers/Transaction");
 
 router.get("/fetchApi", fetchApi);
-// router.get("/listTransactions", listTransactions);
-// router.get("/getStatistics", getStatistics);
-// router.get("/getPriceRangeDistribution", getPriceRangeDistribution);
-// router.get("/getCategoryDistribution", getCategoryDistribution);
 router.post("/getCombinedData", getCombinedData);
 
 module.exports = router;
